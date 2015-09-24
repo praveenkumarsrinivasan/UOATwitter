f7de7b08874c2c794b3d5c2acb56caa24248d453
Praveen Kumar Srinivasan <praveen.sxi@gmail.com>
2015-09-24 13:50:41 +1200

    streaming

--- StreamingData.py
+++ StreamingData.py
@@ -1,7 +1,9 @@
+import time
 from TwitterApp import *
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+
 
 '''
 There are different kinds of streams: public stream, user stream, multi-user(site) streams
@@ -11,7 +13,7 @@ There are different kinds of streams: public stream, user stream, multi-user(sit
 #sets up a connection and runs for the given time
 #http://stats.seandolinar.com/collecting-twitter-data-using-a-python-stream-listener/
 class LimitListener(StreamListener):
-    def __init__(self, start_time, time_limit=60):
+    def __init__(self, start_time, time_limit=60.0):
         self.time = start_time
         self.limit = time_limit
 
@@ -28,7 +30,7 @@ class LimitListener(StreamListener):
                 print 'failed ondata,', str(e)
                 time.sleep(5)
                 pass
-
+        exit()
 
     def on_error(self, status_code):
         print 'An error has occured! Status code = %s' % status_code
@@ -64,19 +66,22 @@ class StdOutListener(tweepy.StreamListener):
 if __name__ == '__main__':
     auth = get_auth()
 
-    track_user_list = ['anjaliscorpio2910']
+    track_user_list = ['anjaligupta2910']
+    track_user_list = ['575930104']
+
     track_keyword_list = ['programming']
 
-    std_listener = StdOutListener()
-    std_stream = tweepy.Stream(auth, std_listener)
+    # std_listener = StdOutListener()
+    # std_stream = tweepy.Stream(auth, std_listener)
     # std_stream.filter(track=track_keyword_list, async=True)
-    std_stream.filter(track=track_keyword_list)
+    # std_stream.filter(track=track_keyword_list)
 
-    # limit_listner = LimitListener(start_time, time_limit=20)
+    # limit_listener = LimitListener(time.time(), time_limit=60.0)
     # limit_stream = Stream(auth, limit_listener) #initialize Stream object with a time out limit
-    # limit_stream.filter(track=keyword_list, languages=['en'])  #call the filter method to run the Stream Object
+    # limit_stream.filter(track=track_keyword_list, languages=['en'])  #call the filter method to run the Stream Object
 
-    # std_listener1 = StdOutListener()
-    # std_stream1 = tweepy.Stream(auth, std_listener)
-    # std_stream1.filter(track_user_list, track_keyword_list)
+    std_listener1 = StdOutListener()
+    std_stream1 = tweepy.Stream(auth, std_listener1)
+    # std_stream1.filter(track=track_keyword_list)
+    std_stream1.filter(follow=track_user_list)
 
